23e86651790b9765cc6ffe77847ffe320dd110fd
Márcio Mingante <marciomvp17@gmail.com>
2024-04-16 16:38:08 -0300

    conclui req 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Search from './components/Search';
 import Album from './components/Album';
+import Layout from './components/Layout';
+import Header from './components/Header';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path="/search" element={ <Search /> } />
-      <Route path="/album/:id" element={ <Album /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+      </Route>
 
       <Route path="*" element={ <h1>404 not dfound</h1> } />
     </Routes>
---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+import LoadingPage from './LoadingPage';
+
+function Header() {
+  const [userName, setUserName] = useState('');
+  const [loadingPage, setLoadingPage] = useState(false);
+
+  useEffect(() => {
+    const handleUserName = async () => {
+      setLoadingPage(true);
+
+      const result = await getUser();
+
+      setLoadingPage(false);
+
+      return setUserName(result.name);
+    };
+
+    handleUserName();
+  }, []);
+
+  return (
+    <header data-testid="header-component">
+      <h2 data-testid="header-user-name">
+        {loadingPage === true && <LoadingPage />}
+        {loadingPage === false && userName}
+      </h2>
+      <NavLink to="/search" data-testid="link-to-search">Pesquisar</NavLink>
+      <NavLink to="/favorites" data-testid="link-to-favorites">Favoritos</NavLink>
+      <NavLink to="/profile" data-testid="link-to-profile">Perfil</NavLink>
+    </header>
+  );
+}
+
+export default Header;
